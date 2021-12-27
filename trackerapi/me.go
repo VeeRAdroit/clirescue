275b86535df3b54dfa447bac5a2b2d2aa02e9812
Veera Raagavan Rajasekaran <veera.rajasekaran@circles.asia>
2021-12-27 10:44:23 +0800

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/VeeRAdroit/clirescue/cmdutil"
+	"github.com/VeeRAdroit/clirescue/user"
 )
 
 var (
